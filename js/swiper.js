d9c0c1bb0772501bcf255eee982ae2087891c1a9
Neku894 <greyhotel2@gmail.com>
2017-10-13 13:01:03 +0300

    first commit

---
+++ js/swiper.js
@@ -0,0 +1,12 @@
+// $$(document).on('pageInit', function (e) {
+// // Do something here when page loaded and initialized
+//   var mySwiper = myApp.swiper('.swiper-container',{
+//     pagination: '.swiper-pagination',
+//     paginationHide: false,
+//     autoplay:3000,
+//     onReachEnd: function(swiper) {
+//       //callback function code here
+//     }
+//   });                
+
+// })
